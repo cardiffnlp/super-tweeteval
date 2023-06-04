f9aba0406ffde33b244f6a1f9ba8b297042665ee
asahi417 <asahi1992ushio@gmail.com>
2023-06-04 23:55:40 +0100

    init

--- baselines/t5/tweet_qa.py
+++ baselines/t5/tweet_qa.py
@@ -237,7 +237,7 @@ def train(model_name: str, model_low_cpu_mem_usage: bool, dataset: str, dataset_
         if os.path.exists(f"{output_dir}/model/evaluation_metrics.json"):
             copyfile(f"{output_dir}/model/evaluation_metrics.json", f"{model_alias}/evaluation_metrics.json")
         sample = [f"context: {i[dataset_column_passage]}, question: {i[dataset_column_question]}" for i in dataset_instance[dataset_split_train]][:3]
-        widget = "\n".join([f"- text: {t}\n  example_title: example {_n + 1}" for _n, t in enumerate(sample)])
+        widget = "\n".join([f"- text: '{t}'\n  example_title: example {_n + 1}" for _n, t in enumerate(sample)])
         with open(f"{model_alias}/README.md", "w") as f:
             f.write(f"""
 ---
