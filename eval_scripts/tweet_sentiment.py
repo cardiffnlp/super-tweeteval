23d8d2ba1b8155db6753ce8da6c20c3c085f8a62
asahi417 <asahi1992ushio@gmail.com>
2023-06-07 14:48:10 +0100

    init

--- eval_scripts/tweet_sentiment.py
+++ eval_scripts/tweet_sentiment.py
@@ -20,7 +20,7 @@ label_names = data.features['gold_label'].names
 
 with open(opt.prediction_file) as f:
     output = [i for i in f.read().split("\n")]
-    predictions = [label_names.index(x) for x in output if x in label_names]
+    predictions = [label_names.index(x) if x in label_names else -1 for x in output ]
     
 # metric: macro_averaged_mean_absolute_error
 # from: https://github.com/scikit-learn-contrib/imbalanced-learn/blob/master/imblearn/metrics/_classification.py 
@@ -30,8 +30,6 @@ labels = [0, 1, 2, 3, 4]
 mae = []
 sample_weight = np.ones(gold_labels.shape)
 for possible_class in labels:
-    print(len(gold_labels), len(predictions))
-    input()
     indices = np.flatnonzero(gold_labels == possible_class)
     mae.append(
         mean_absolute_error(
